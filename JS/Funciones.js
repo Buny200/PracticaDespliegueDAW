94f6b6e49785d9817bf308f93b05e02a6555e841
VictorHeredia <Victorinlamuela@gmail.com>
2022-11-12 16:02:46 +0100

    commitDespliegue

---
+++ JS/Funciones.js
@@ -0,0 +1,19 @@
+function formularioContacto() {
+
+    let nombre;
+    let apellido;
+    let dni;
+    let fechaNace;
+    let codigoPostal;
+    let direccion;
+
+    nombre = document.getElementById("nombre").value
+    apellido = document.getElementById("apellido").value.
+    dni = document.getElementById("dni").value.
+    fechaNace = document.getElementById("nacimiento").value.
+    codigoPostal = document.getElementById("codigoPostal").value.
+    direccion = document.getElementById("direccion").value.
+   
+            alert("Hemos recogido correctamente la información, nos pondremos en contacto pronto");
+           
+}
